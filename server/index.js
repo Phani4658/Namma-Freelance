2f737bf0e77d4ea1feda5d8e6e254cb7d6a6411a
Phani <officialphani.cse@gmail.com>
2024-05-12 01:58:07 +0530

    Add Services and Profile APIs

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const connection = require("./config/db");
 const { authRouter } = require("./routes/auth.routes");
+const { profileRouter } = require("./routes/profile.routes");
+const { serviceRouter } = require("./routes/services.routes");
 
 const app = express();
 const port = 3008;
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/",serviceRouter);
 
 app.listen(port, () => {
   console.log(`server is listening on http://localhost:${port}`);
